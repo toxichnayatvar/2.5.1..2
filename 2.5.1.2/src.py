c0424c1aaca4ac02d614fd642fed3878b4fbb10b
toxichnayatvar <123656439+toxichnayatvar@users.noreply.github.com>
2023-04-10 23:55:45 +0300

    Add files via upload

---
+++ 2.5.1.2/src.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.fillcolor('#32CD32')
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(70)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(90)
+    turtle.left(90)
+
+
+turtle.done()
